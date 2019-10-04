8c66b2f0c9e2fa63c291cbe5f449db52d0d50d31
IT <it@corporate-happiness.de>
2019-10-04 11:06:15 +0200

    Plugin: LenzOrderPositionComment removed & CohaCart Button added

--- custom/plugins/LenzOrderPositionComment/Resources/frontend/js/plugin.js
+++
@@ -1,9 +0,0 @@
-$.subscribe('plugin/swStorageField/setFieldValueFromStorage.lenzOrderPositionComment', function() {
-	var me = arguments[1];
-
-	var value = me.storage.getItem(me.storageKey);
-
-	if(value === null) {
-		$(me.$el.attr('data-selector')).val(me.$el.val());
-	}
-});
