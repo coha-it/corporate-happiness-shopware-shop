05c347f5c6bbba1e3b481b20357880a48e5b58e9
IT <it@corporate-happiness.de>
2020-11-23 13:18:36 +0100

    Shopware Update Nov 2020

--- engine/Shopware/Kernel.php
+++ engine/Shopware/Kernel.php
@@ -98,9 +98,9 @@ class Kernel extends SymfonyKernel
      * Is available in the DIC as parameter 'shopware.release.*' or a Struct containing all the parameters below.
      */
     protected $release = [
-        'version' => '5.6.8',
+        'version' => '5.6.9',
         'version_text' => '',
-        'revision' => '202008121209',
+        'revision' => '202011060821',
     ];
 
     /**
